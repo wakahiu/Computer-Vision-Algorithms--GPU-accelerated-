2a8e326b9918b23cde85d0991ee0fdbe8bd36f12
wakahiu <wakahiu@berkeley.edu>
2014-04-28 12:40:31 -0400

    Gray scale GPU done

--- src/commonHeaders.h
+++ src/commonHeaders.h
@@ -12,14 +12,15 @@
 #include <string>
 #include <math.h>
 #include <algorithm>
+#include <cuda.h>
 
 //OpenCV is modular
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>		//IO ops
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/nonfree/features2d.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
+//#include <opencv2/features2d/features2d.hpp>
+//#include <opencv2/nonfree/features2d.hpp>
+//#include <opencv2/nonfree/nonfree.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 
@@ -27,5 +28,17 @@ using namespace std;
 using namespace cv;
 
 #define ESC 27
+
+// handy error macro:
+#define GPU_CHECKERROR( err ) (gpuCheckError( err, __FILE__, __LINE__ ))
+inline static void gpuCheckError( cudaError_t err,
+                          const char *file,
+                          int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+               file, line );
+        exit( EXIT_FAILURE );
+    }
+}
 
 #endif
